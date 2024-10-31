b535c7eaf3a15a02d3719a83bd10a275ec95f975
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-11-01 01:55:57 +0600

    Book Card And Responsive Card Layout added

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 
 const Book = ({ book }) => {
-  const { bookId, author, bookName } = book;
+  const {
+    bookId,
+    author,
+    bookName,
+    category,
+    image,
+    publisher,
+    rating,
+    review,
+  } = book;
   return (
-    <div>
-      <h1>{bookName}</h1>
+    <div className="card bg-base-100 w-96 shadow-xl p-7">
+      <figure className="bg-[#F3F3F3] rounded-2xl">
+        <img src={image} alt={bookName} className="h-44 py-7 " />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {bookName}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>By: {author}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -11,7 +11,7 @@ const Books = () => {
   return (
     <div className="my-6">
       <h1 className="text-3xl text-center">Books</h1>
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
         {allBooks.map((book) => (
           <Book key={book.bookId} book={book} />
         ))}
